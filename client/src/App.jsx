1cbf80ccec9d98ef347749b75db7f08837fdfe9a
RayaneC-01 <rchaabane20@gmail.com>
2026-08-06 21:22:21 +0200

    fix(App): placer Router au dessus de AuthProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,11 @@ import "./index.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-//barre de recherche + filtres
 import SearchBar from "./components/SearchBar";
-//ajouter une recette
 import AddRecipe from "./pages/AddRecipe";
 import RecipeDetail from "./pages/RecipeDetail";
 import EditRecipe from "./pages/EditRecipe";
 
-// Route principale de l'application, encapsulée dans le AuthProvider pour gérer l'état d'authentification global.
-// Le Router est utilisé pour gérer la navigation entre les différentes pages de l'application, puis les Routes définissent les chemins et les composants correspondants.
 function App() {
   return (
     <Router>
